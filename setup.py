aa1466793798faddef23a41395c320353c01e745
Michael Ismail Febrian <68809822+michaelifebrian@users.noreply.github.com>
2024-12-23 17:16:48 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Lukas Blecher",
     author_email="lblecher@meta.com",
-    url="https://github.com/facebookresearch/nougat",
+    url="https://github.com/michaelifebrian/nougat",
     license="MIT",
     packages=find_packages(
         exclude=[
@@ -43,7 +43,7 @@ setup(
     py_modules=["predict", "app", "train", "test"],
     python_requires=">=3.7",
     install_requires=[
-        "transformers>=4.25.1",
+        "transformers==4.38.2",
         "timm==0.5.4",
         "orjson",
         "opencv-python-headless",
